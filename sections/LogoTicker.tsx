1b67a871887d7b3ed186a301f92d579ebede21f5
nischal puri <mrnischalpuri@gmail.com>
2025-01-12 16:25:18 +0545

    [logo tickers]

--- sections/LogoTicker.tsx
+++ sections/LogoTicker.tsx
@@ -1,3 +1,37 @@
+import QuantumLogo from "@/assets/logo-quantum.png"
+import CelestialLogo from "@/assets/logo-celestial.png"
+import AcmeLogo from "@/assets/logo-acme.png"
+import EchoLogo from "@/assets/logo-echo.png"
+import PulseLogo from "@/assets/logo-pulse.png"
+import ApexLogo from "@/assets/logo-apex.png"
+import Image from "next/image"
 export const LogoTicker = () => {
-  return null;
-};
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex justify-center overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image src={AcmeLogo} alt="acme logo" className="logo-ticker-img" />
+            <Image
+              src={QuantumLogo}
+              alt="quantum logo"
+              className="logo-ticker-img"
+            />
+            <Image src={EchoLogo} alt="echo logo" className="logo-ticker-img" />
+            <Image
+              src={CelestialLogo}
+              alt="celestial logo"
+              className="logo-ticker-img"
+            />
+            <Image
+              src={PulseLogo}
+              alt="pulse logo"
+              className="logo-ticker-img"
+            />
+            <Image src={ApexLogo} alt="apex logo" className="logo-ticker-img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
